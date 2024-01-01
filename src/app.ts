484f71f59451894b0534dfd054b830eed4ceb07b
wuhewuhe <dachichiwuhe@gmail.com>
2024-01-02 00:21:40 +0100

    example

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,18 @@
 // app.ts
-export const add = (a: number, b: number): number => a+b;
+export const add = (a: number, b: number): number => a + b;
 console.log(add(3, 5)); // Output: 8
+
+interface CatInfo {
+  age: number;
+  breed: string;
+}
+
+type CatName = "miffy" | "boris" | "mordred";
+
+const cats: Record<CatName, CatInfo> = {
+  miffy: { age: 10, breed: "Persian" },
+  boris: { age: 5, breed: "Maine Coon" },
+  mordred: { age: 16, breed: "British Shorthair" },
+};
+
+console.log(cats.boris)
